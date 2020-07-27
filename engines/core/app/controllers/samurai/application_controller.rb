4a222a36a13272fb38be19702cd6da3c55bf993a
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-27 22:28:22 +0700

    Reorganize the controllers folder

--- engines/core/app/controllers/samurai/application_controller.rb
+++ engines/core/app/controllers/samurai/application_controller.rb
@@ -1,4 +1,4 @@
-module Core
+module Samurai
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
   end
--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,5 @@
 Samurai::Core::Engine.routes.draw do
 end
 Rails.application.routes.draw do
+    mount Samurai::Core::Engine => "/", as: 'samurai'
 end
