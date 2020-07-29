eef380e71f7229dc4d9d6b4489c709d79a175c12
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-29 22:26:05 +0700

    Add migrations to parent app paths

--- engines/core/lib/samurai/core/engine.rb
+++ engines/core/lib/samurai/core/engine.rb
@@ -6,7 +6,16 @@
 module Samurai
   module Core
     class Engine < ::Rails::Engine
-    isolate_namespace Samurai
+      isolate_namespace Samurai
+
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match(root.to_s)
+        config.paths["db/migrate"].expanded.each do |p|
+        app.config.paths["db/migrate"] << p
+        end
+        end
+      end
+      
     end
   end
 end
