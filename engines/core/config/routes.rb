11b224fb91845de95ad0562eaf406c2aead4c7bc
tanhongit <phuongtan12357nguyen@gmail.com>
2020-08-04 13:47:38 +0700

    done step 5,6,7,8,9

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,3 +1,4 @@
 Samurai::Core::Engine.routes.draw do
+    devise_for :users, class_name: "Samurai::Core::User", module: :devise
     root to: "dashboard#index"
 end
--- engines/core/lib/samurai/core/engine.rb
+++ engines/core/lib/samurai/core/engine.rb
@@ -2,6 +2,15 @@ module Samurai
   module Core
     class Engine < ::Rails::Engine
       isolate_namespace Core
+
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match(root.to_s)
+          config.paths["db/migrate"].expanded.each do |p|
+            app.config.paths["db/migrate"] << p
+          end
+        end
+      end
+
     end
   end
 end
