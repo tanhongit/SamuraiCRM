58bb9552509d7d0cafbf6bf3aa7c00ec85249ae4
tanhongit <phuongtan12357nguyen@gmail.com>
2020-08-04 15:26:45 +0700

    done step 1-> 7

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,7 @@
 Samurai::Core::Engine.routes.draw do
     devise_for :users, class_name: "Samurai::Core::User", module: :devise
+    namespace :admin do
+        get '/' => 'admin#index'
+    end
     root to: "dashboard#index"
 end
