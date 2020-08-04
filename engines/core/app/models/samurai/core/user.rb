79b30bfbf76bc923a880722f92746d640a99833d
tanhongit <phuongtan12357nguyen@gmail.com>
2020-08-04 16:02:00 +0700

    none

--- engines/core/app/models/samurai/core/user.rb
+++ engines/core/app/models/samurai/core/user.rb
@@ -1,8 +1,10 @@
 module Samurai::Core
-  class User < ApplicationRecord
+  class User < ActiveRecord::Base
       # Include default devise modules. Others available are:
       # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-      devise :database_authenticatable, :registerable,
-             :recoverable, :rememberable, :validatable
+      devise  :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :trackable, :validatable
+
+      scope :ordered, -> { order('created_at desc') } # NEW SCOPE
   end
 end
