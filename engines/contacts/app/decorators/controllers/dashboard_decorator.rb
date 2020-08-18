4962dc6c35c61b32a590fc58f9e3adde1476e32a
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-18 12:18:22 +0700

    limit ordered contact dashboard

--- engines/contacts/app/decorators/controllers/dashboard_decorator.rb
+++ engines/contacts/app/decorators/controllers/dashboard_decorator.rb
@@ -2,6 +2,6 @@ Samurai::DashboardController.class_eval do
     before_action :set_contacts, only: :index
     private
     def set_contacts
-        @contacts = current_user.contacts
+        @contacts = current_user.contacts.ordered.limit(3)
     end
 end
