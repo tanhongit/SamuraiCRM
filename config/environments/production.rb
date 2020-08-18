369c4907651e997deade7fdddc9f43971f0b5d3b
tanhongit <phuongtan12357nguyen@gmail.com>
2020-08-18 18:16:57 +0700

    config.enable_dependency_loading = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.enable_dependency_loading = true
 end
